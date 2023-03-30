c5fbbb8c2cf1844982151379d60751cabbc828ed
Aleksandr Falin <saneksa01@gmail.com>
2023-03-30 23:19:03 +0300

    1 часть Открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import createObjects from './data.js';
 import render from './rendering.js';
-import './full-size-image.js';
+import addListenersForGallery from './full-size-image.js';
 const data = createObjects();
 
 render(data);
-
-export {data};
+addListenersForGallery(data);
